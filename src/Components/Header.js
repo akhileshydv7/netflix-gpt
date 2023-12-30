67fff44113f06c48f908190c35188430597947e9
Akhilesh Yadav <akhileshyadav74587@gmail.com>
2023-12-30 22:22:46 +0530

    Sign In/Sign Out Api done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import { LOGO_URL } from '../Utils/Constants'
+import { Avatar, LOGO_URL } from '../Utils/Constants'
+import { signOut } from 'firebase/auth';
+import { auth } from '../Utils/firebase';
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 
 const Header = () => {
+    const navigate = useNavigate();
+    const user = useSelector(store => store.user)
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            navigate("/");
+        }).catch((error) => {
+            // An error happened.
+            navigate("/error");
+        });
+    }
     return (
-        <div className='absolute flex justify-between px-8 py-2 w-fit bg-gradient-to-b from-black z-10'>
+        <div className='absolute flex justify-between px-8 py-2 w-screen bg-gradient-to-b from-black z-10'>
             <img className='w-48' src={LOGO_URL}
                 alt="logo"
             />
+            {
+                (user && <div className='flex p-2'>
+                    <img className='w-12 h-12 pt-3' src={Avatar}
+                        alt="usericon" />
+                    <button onClick={handleSignOut} className='font-bold text-white'>(Sign Out)</button>
+                </div>)
+            }
+
         </div>
     )
 }
