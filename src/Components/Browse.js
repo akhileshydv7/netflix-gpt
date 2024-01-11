1064f87a18e198bea7ecad3fe0fe53588415850c
Akhilesh Yadav <akhileshyadav74587@gmail.com>
2024-01-11 22:06:55 +0530

    navbar fixed and updated.

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -26,7 +26,6 @@ const Browse = () => {
                     <SecondaryContainer />
                 </>
             )}
-
         </div>
     )
 }
--- src/Components/SecondaryContainer.js
+++ src/Components/SecondaryContainer.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
     const movies = useSelector(store => store.movies);
+    // console.log(movies);
 
     return movies.nowPlayingMovies && (
         <div className='bg-black'>
